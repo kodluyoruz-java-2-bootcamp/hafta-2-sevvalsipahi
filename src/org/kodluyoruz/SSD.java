55fc6c83ae2e0cd3c2a56df1d3cdf8166b4e4856
sevvalsipahi <38524640+sevvalsipahi@users.noreply.github.com>
2020-07-10 23:37:14 +0300

    Update SSD.java

--- src/org/kodluyoruz/SSD.java
+++ src/org/kodluyoruz/SSD.java
@@ -1,5 +1,4 @@
 package org.kodluyoruz;
-
 /**
  * Bu sınıf bir SSD'yi temsil eder.
  * TODO Bu sınıfı Hardware sınıfının alt sınıfı haline getirin.
@@ -9,12 +8,45 @@ package org.kodluyoruz;
  *
  * TODO Bu özellik ve metotları için gereken kodları bu sınıfın içine yazın
  */
-public class SSD
+public class SSD extends Hardware
 {
 
-    /*
+ private int memory;
+ public int getMemory() {
+  return memory;
+ }
+
+ public void setMemory(int memory) {
+  this.memory = memory;
+ }
+
+ /*
      * Eğer SSD'nin hafızası 750 GB'tan fazlaysa, her 250 GB için fiyatı 275 TL artar.
      * TODO buna göre Hardware sınıfındaki fiyat hesaplayan metodu bu sınıfta yeniden yazın
      */
+
+
+ @Override
+ public double calculateprice()
+ {
+  if(memory>750)
+  {
+   int memorypiece=((memory-750)/250);
+   for(int i=0; i>memorypiece;i++)
+    price = price + 275;
+
+  }
+
+  return price;
+ }
+
+
+
+
+
+
+
+
+
 
 }
