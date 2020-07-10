4f20744bec1755010529cfb5a84ea07493822ee6
sevvalsipahi <38524640+sevvalsipahi@users.noreply.github.com>
2020-07-10 23:38:10 +0300

    Update Motherboard.java

--- src/org/kodluyoruz/Motherboard.java
+++ src/org/kodluyoruz/Motherboard.java
@@ -11,13 +11,85 @@ package org.kodluyoruz;
  *
  * TODO Bu 3 özellik ve bunların metotları için gereken kodları bu sınıfın içine yazın
  */
-public class Motherboard
+public class Motherboard extends Hardware
 {
+
+
+    private String cpuArchitect;
+    private boolean wifiIncluded;
+    private boolean bluetoothIncluded;
+
+
+
+
+    public String getCpuArchitect() {
+        return cpuArchitect;
+    }
+
+    public void setCpuArchitect(String cpuArchitect) {
+        this.cpuArchitect = cpuArchitect;
+    }
+
+    public boolean isWifiIncluded() {
+        return wifiIncluded;
+    }
+
+    public void setWifiIncluded(boolean wifiIncluded) {
+        this.wifiIncluded = wifiIncluded;
+    }
+
+    public boolean isBluetoothIncluded() {
+        return bluetoothIncluded;
+    }
+
+    public void setBluetoothIncluded(boolean bluetoothIncluded) {
+        this.bluetoothIncluded = bluetoothIncluded;
+    }
 
     /*
      * Eğer anakartın üzerinde WiFi chipset'i varsa anakartın fiyatı 250 TL artar.
      * Eğer anakartın üzerinde bluetooth chipset'i varsa anakartın fiyatı 100 TL artar.
      * TODO buna göre Hardware sınıfındaki fiyat hesaplayan metodu bu sınıfta yeniden yazın
      */
+
+
+
+@Override
+public double calculateprice()
+{
+
+   if(wifiIncluded==true)
+    {
+        price+=250;
+
+    }
+
+     if(bluetoothIncluded==true)
+    {
+        price+=100;
+
+    }
+
+    return price;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
