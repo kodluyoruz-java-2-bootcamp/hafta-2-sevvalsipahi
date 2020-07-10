609690d3a9e4c0d1e5d106ebc049aa32d915bc0d
sevvalsipahi <38524640+sevvalsipahi@users.noreply.github.com>
2020-07-10 23:39:17 +0300

    Update Computer.java

--- src/org/kodluyoruz/Computer.java
+++ src/org/kodluyoruz/Computer.java
@@ -13,46 +13,108 @@ package org.kodluyoruz;
  */
 public class Computer
 {
+    public Computer()
+    {
+
+    }
+    public Computer(Hardware motherboard) {
+        this.motherboard = motherboard;
+    }
+
+
 
     /**
      * anakart
      */
     private Hardware motherboard;
 
+    public Hardware getMotherboard() {
+        return motherboard;
+    }
+
+    public void setMotherboard(Hardware motherboard) {
+        this.motherboard = motherboard;
+    }
+
+
     /**
      * işlemci
      */
     private Hardware cpu;
+    public Hardware getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(Hardware cpu) {
+        this.cpu = cpu;
+    }
 
     /**
      * RAM
      */
     private Hardware ram;
+    public Hardware getRam() {
+        return ram;
+    }
+
+    public void setRam(Hardware ram) {
+        this.ram = ram;
+    }
+
 
     /**
      * SSD
      * İsteğe bağlı: Her bilgisayarda SSD olmayabilir
      */
     private Hardware ssd;
+    public Hardware getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(Hardware ssd) {
+        this.ssd = ssd;
+    }
 
     /**
      * ekran kartı
      * İsteğe bağlı: Her bilgisayarda ekran kartı olmayabilir
      */
     private Hardware gpu;
+    public Hardware getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(Hardware gpu) {
+        this.gpu = gpu;
+    }
+
+
+
+
+
 
     public double getTotalPrice()
     {
         // Bu metot bilgisayarın donanımlarına bakarak toplam fiyatını hesaplar.
         // TODO metodu doldurun
-        return 0.0;
+
+        double totalprice=cpu.calculateprice()+gpu.calculateprice()+motherboard.calculateprice()+ram.calculateprice()+ssd.calculateprice();
+        System.out.println(totalprice);
+        // TODO metodu doldurun
+        return totalprice;
+      //  return this.cpu.getPrice() + this.gpu.getPrice() + this.motherboard.getPrice() + this.ram.getPrice() + this.ssd.getPrice();
+
     }
 
     public int getTotalPower()
     {
-        // Bu metot bilgisayarın donanımlarına bakarak toplamda ne kadar güç tüketeceğini hesaplar.
+
+        //return this.cpu.getPower() + this.gpu.getPower() + this.motherboard.getPower() + this.ram.getPower() + this.ssd.getPower();
+
+        int totalpower=cpu.power+gpu.power+motherboard.power+ram.power+ssd.power;
+        System.out.println(totalpower);
         // TODO metodu doldurun
-        return 0;
+        return totalpower;
     }
 
 }
